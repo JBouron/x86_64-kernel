1e3d9083dd940e2923152b9677bd1eb309da8357
Justinien Bouron <justinien.bouron@gmail.com>
2023-06-13 20:09:51 -0700

    Bootloader/stage1: When parsing the memory map from BIOS, check that the map follows the assumptions made in this bootloader.
    The BIOS function to get the memory map (INT=15h AX=E820h) does not
    guarantee that the returned map is sorted nor does it guarantee that
    ranges of the map are not overlapping.
    In practice though, it seems that the map is always sorted and never
    contains overlapping entries. To keep things simple, the bootloader
    assumes this, for now. Hence, when parsing the map, check that those
    assumptions hold and panic if that is not the case.
    If in the future a machine/VM triggers this panic, the parsing code
    would need to be changed to both sort the map and merge overlapping
    ranges.

--- bootloader/stage1/memmap.asm
+++ bootloader/stage1/memmap.asm
@@ -2,6 +2,7 @@
 
 %include "malloc.inc"
 %include "bioscall.inc"
+%include "arith64.inc"
 
 SECTION .data
 ; Number of entries in the memory map.
@@ -115,6 +116,147 @@ DEF_GLOBAL_FUNC(parseMemoryMap):
     mov     eax, [ebp - 0x8]
     mov     [memoryMapLen], eax
 
+    ; FIXME: Here we are making two assumptions:
+    ;   i) The memory map, as reported by the BIOS is sorted on the base address
+    ;   of the ARDS.
+    ;   ii) No two elements of the memory map are overlapping.
+    ; In practice those assumptions seem to hold. However per the BIOS
+    ; function's specification, the BIOS is not guaranteeing those.
+    ; At some point we should therefore sort and merge adjacent ranges. For now
+    ; we simply make a sanity check that those assumptions are holding.
+    push    DWORD [memoryMapLen]
+    push    DWORD [memoryMap]
+    call    checkMemoryMapInvariants
+    add     esp, 8
+    test    eax, eax
+    jnz     .invariantsOk
+    ; Assumptions did not hold.
+    LOG     "Memory map is either not sorted or has overlapping ranges"
+.dead:
+    hlt
+    jmp     .dead
+.invariantsOk:
+
     pop     ebx
+    leave
+    ret
+
+; ==============================================================================
+; Check that the memory map given as argument follows both invariants:
+;   i ) All entries are sorted on their base address.
+;   ii) No entries are overlapping.
+; @param (DWORD): Array of entries.
+; @param (DWORD): Number of entries.
+checkMemoryMapInvariants:
+    push    ebp
+    mov     ebp, esp
+
+    push    ebx
+    push    esi
+
+    ; Memory maps of length 1 are naturally following the invariants.
+    cmp     DWORD [ebp + 0xc], 1
+    je      .out
+
+    ; EBX = Pointer to entries.
+    mov     ebx, [ebp + 0x8]
+    ; ESI = Iteration counter.
+    xor     esi, esi
+
+    ; For i = 0; i < len(map)-1; ++i;
+.loopCond:
+    mov     eax, [ebp + 0xc]
+    dec     eax
+    cmp     esi, eax
+    je      .loopOut
+.loopStart:
+    ; Check that map[i].base < map[i+1].base
+    ; EAX = &map[i].base
+    mov     eax, esi
+    imul    eax, 24
+    add     eax, ebx
+    ; Second operand for cmp64
+    push    eax
+    ; EAX = &map[i+1].base. Computing this is easier, just add the size of an
+    ; entry since EAX already points to the previous entry's base address.
+    add     eax, 24
+    ; First operand for cmp64
+    push    eax
+
+    call    cmp64
+    add     esp, 8
+    cmp     eax, 1
+    je      .expectedRes
+    ; The base address of map[i+1] is not > map[i].base. This is a violation of
+    ; our assumption. Return false.
+    LOG     "map[i].base >= map[i+1].base for i = $", esi
+    xor     eax, eax
+    jmp     .out
+.expectedRes:
+    ; We have map[i].base < map[i+1].base. Now check that map[i] and map[i+1]
+    ; do not overlap; we do this by making sure that:
+    ;   map[i].base + map[i].len <= map[i+1].base
+    ; First compute map[i].base + map[i].len.
+    ; Make some space for the result of the add.
+    sub     esp, 8
+    ; EAX = &map[i].base
+    mov     eax, esi
+    imul    eax, 24
+    add     eax, ebx
+    ; Push second add operand.
+    push    eax
+    ; EAX = &map[i].len
+    add     eax, 8
+    ; Push first operand for add64.
+    push    eax
+    ; Push destination for add64, allocated above.
+    lea     eax, [esp + 8]
+    push    eax
+    ; Add the 64-bit values.
+    call    add64
+    ; Clear args (3).
+    add     esp, 12
+    ; Now ESP points to the 64-bit value containing the result of map[i].base +
+    ; map[i].len. Compare it against map[i+1].base.
+    ; Push cmp64 second operand, e.g. map[i].base + map[i].len
+    push    esp
+    ; EAX = &map[i+i].base
+    mov     eax, esi
+    inc     eax
+    imul    eax, 24
+    add     eax, ebx
+    ; Push first cmp64 operand
+    push    eax
+
+    call    cmp64
+    ; Clear args (2).
+    add     esp, 8
+
+    ; We want either:
+    ;   * map[i+1].base > map[i].base + map[i].len
+    ;   * map[i+1].base == map[i].base + map[i].len
+    ; In other words the result of the cmp64 should either be 1 or 0 (given that
+    ; map[i+1].base was the first operand), so NOT -1.
+    cmp     eax, -1
+    jne     .expectedRes2
+    ; We have map[i+1].base < map[i].base + map[i].len. Invariant violated.
+    LOG     "map[i+1].base < map[i].base + map[i].len for i = $", esi
+    xor     eax, eax
+    jmp     .out
+.expectedRes2:
+    ; The ranges are not overlapping. Check the next pair of range.
+    ; Clear the space allocated on the stack for the add64.
+    add     esp, 8
+    ; fall-through.
+.loopNext:
+    inc     esi
+    jmp     .loopCond
+.loopOut:
+    ; All ranges are good, success return true.
+    mov     eax, 1
+.out:
+    pop     esi
+    pop     ebx
+
     leave
     ret
